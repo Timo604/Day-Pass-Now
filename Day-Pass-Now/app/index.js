add31f6d6cb4e8f2fe890634120e421e0c6af537
Timo604 <46822011+Timo604@users.noreply.github.com>
2019-12-03 13:40:23 -0600

    try to use the serverside google APIs  - quitting

--- Day-Pass-Now/app/index.js
+++ Day-Pass-Now/app/index.js
@@ -1,21 +1,28 @@
+
+
 function findGyms(){ 
+    
     "use strict";
  
- //reference to gymOutputs in DOM
- let gymOutput = document.querySelector('.gym-output');
  //find gyms button
  let findGymBtn = document.getElementById("find");
- 
+ //location searchbar
  let locationInput = document.getElementById("ls");
- 
+ //current location
  let currentLocBtn = document.getElementById("cl");
 
  //variables for google APIs
  var geocoder;
- var map;
  var location;
+ var map;
 
 
+ var data;
+ var isSuccessful;
+ var places;
+ var detailed;
+
+ 
  //event listener on location test field to clear on click
  locationInput.addEventListener('click', () => {
      locationInput.value = '';
@@ -29,29 +36,35 @@ function findGyms(){
  })
  
  //event listener on find gyms button
- findGymBtn.addEventListener('click', () => {
+ findGymBtn.addEventListener('click', async function(){
     console.log('find button clicked');
     var radius = document.getElementById("dis").value * 1609.34 //approximate miles to meters conversion
-    var loc = codeAddress({'address':locationInput.value});
-    var places = requestPlaces(adr);
-    var details = requestDetails(places);
-    displayGyms(locationInput.value, document.getElementById("dis").value, gymOutput);
- })
+    //get latitude and longitude object 
+    $.post("places", {'adr': locationInput.value, 'rad': String(radius)}, function(response){
+    places = response;
+    console.log("places posted");
+    }).then(displayGyms(places));    
+    });  
 
- function codeAddress(adr){
-    var geocoder = new google.maps.Geocoder();
-    geocoder.geocode(adr, function(results, status) {
-        if(status == 'OK'){
-            location = results[0].geometry.location;
-            console.log("location created... " + location);
-            return location;
-        }
-        else{
-            alert('Geocode was not successful because: ' + status);
-        }
-    });
-}
- 
+
+/*function codeAddress(adr){
+    console.log(adr);
+    return new Promise((resolve, reject) => {
+        geocoder = new google.maps.Geocoder();
+        geocoder.geocode({'address' : adr}, (results, status) => {
+            console.log(status);
+            if(status == 'OK'){
+                location = results[0].geometry.location;
+                console.log("the lat is " + location.lat());
+               resolve(location);
+            }
+            else{
+                reject(console.log("something went wrong at geocoder" + status));
+            }
+        });
+    }); 
+    }*/
+  
  
  //event lsitener for enter key
  //currently displays for split second then page refreshes
@@ -63,30 +76,41 @@ function findGyms(){
          displayGyms(locationInput.value, document.getElementById("dis").value, gymOutput);
      }
  })*/
- }
  
- function displayGyms(gymName, distance, output){
-     clearGyms(output);
-     //create p node
-     let p = document.createElement('p');
-     //text is the location input value
-     let locationText = document.createTextNode("Gyms within " + 
-         distance + " of " +gymName);
-     //append the location text
-     p.appendChild(locationText);
-     output.appendChild(p);
- }
  
- function clearGyms(output){
-     let gyms = output.querySelectorAll('p')
+ function displayGyms(p){
+    var gymOutput = document.getElementById('gym-output');
+     clearGyms(gymOutput);
+     $.each(p, ()=>{
+        $.post("details", this, function(response){
+        detailed = response;
+        console.log(detailed);
+
+        //create p node
+        let p = document.createElement('p');
+        //text is the location input value
+        let locationText = document.createTextNode(detailed.name + " is within " + document.getElementById("dis").value +  " of " + locationInput.value);
+        //append the location text
+        p.appendChild(locationText);
+        gymOutput.appendChild(p);
+        }); 
+     });
+    }
+     
+ 
+ 
+ function clearGyms(gymOutput){
+     let gyms = gymOutput.querySelectorAll('p')
      for(let gym of gyms)
      {
          gym.remove();
      }
      console.log("gyms cleared");
  }
+}
 
  function initMap(){
+    //initializes map and places once the api is loaded -- callback function
     //location of Chicago
     var chicago = {lat: 41.8781, lng: -87.6298};
     //the map, centered on Chicago as default
@@ -96,11 +120,14 @@ function findGyms(){
         center: chicago,
         fullscreenControl: false,
         streetViewControl: false});
-         
     var marker = new google.maps.Marker({position: chicago, map: map});
+
+    //add google autocomplete api to locationInput for addresses
+    var options = {types: ['address']};
+    autocomplete = new google.maps.places.Autocomplete(document.getElementById("ls"), options);
  }
 
- 
+
 
  //on start-up
  findGyms();
--- Day-Pass-Now/server.js
+++ Day-Pass-Now/server.js
@@ -1,6 +1,7 @@
 /* a simple Express server for Node.js*/
 var express = require("express"),
 http = require("http"),
+fetch = require('node-fetch'),
 appTest;
 
 //adding body-parser for JSON parsing
@@ -15,23 +16,46 @@ appTest.use(bodyParser.urlencoded({extended: false}));
 
 
 var location;
-var Places = {
-    "places":[]
-}
+var places = []
+var detailed = []
 
+appTest.post("/places", function(req, res){
+    
+    location = requestLocation(req.adr);
+    places = requestPlaces(location, req.rad);
+    detailed = requestDetails(places);
+    res.json(detailed);
+    //console.log(places);
+});
 
-function requestPlaces(adr){
-  var url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?location=' + adr + '&rankby=distance&type=gym&key=AIzaSyDueDNPRDxUx0PR3fC08vmq0VkTP8mkz14';
-  $.getJSON(url, function(result){
-      console.log(result)
-  });
-  return result;
-}
+//appTest.post("/details", function(req, res){
+ //   requestDetails(req);
+   // res.json(detailed);
+    //console.log("got through details");
 
+//});
 function requestDetails(places){
-    $.each(places, function(i, item){
-        var placeID =places[i].place_id 
-        console.log(placeID);
-        var url = 'https://maps.googleapis.com/maps/api/place/details/json?place_id=' + placeID + '&fields=name,geometry/location,website&key=AIzaSyDueDNPRDxUx0PR3fC08vmq0VkTP8mkz14'
-        })
+    for(var i = 0; i < places.length(); i++){
+        placeID = place.place_id;
+        url = 'https://maps.googleapis.com/maps/api/place/details/json?place_id=' + placeID + '&fields=name,geometry/location,website&key=AIzaSyDueDNPRDxUx0PR3fC08vmq0VkTP8mkz14';
+        fetch(url)
+            .then(res => detailed.push(res));
+    }
+    return detailed;
+}
+
+function requestPlaces(location, radius){
+    url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input='gym'&inputtype='textquery'&fields=place_id&locationbias=circle:" + String(radius) + "@" + location+ "&key=AIzaSyDueDNPRDxUx0PR3fC08vmq0VkTP8mkz14";
+    console.log(url);
+   fetch(url)
+   .then(res => res.json())
+   .then(json => console.log(json));
+   
+}
+
+function requestLocation(adr){
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + adr + ",&key=AIzaSyDueDNPRDxUx0PR3fC08vmq0VkTP8mkz14";
+    fetch(url)
+            .then(res => res.json())
+            .then(json=> console.log(json));
 }
